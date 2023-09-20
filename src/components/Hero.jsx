d701c6f0fd88bb47b958cd58785d1728099da75e
wbisoles1214 <wbisoles@gmail.com>
2023-09-20 16:45:34 +0800

    i added earth gif

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-
+import { BsArrowRight } from 'react-icons/bs';
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
 
@@ -10,15 +10,15 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-5 h-5 rounded-full" />
+          <div className="w-1 sm:h-80 h-40 " />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Webster</span>
+          <h1 className={`${styles.heroHeadText} text-purple-500`}>
+            Hi, I'm <span className="text-[#5eb7ff]">Webster</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} mt-2 text-cyan-500`}>
             I'm a FullStack Web Developer <br className="sm:block hidden" />
           </p>
         </div>
@@ -29,8 +29,9 @@ const Hero = () => {
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="https://wbisoles1214.github.io/">
           <div>
-            <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full">
-              Profile
+            <button className="bg-gradient-to-b from-blue-500 to-blue-700 text-white font-semibold py-2 px-4 rounded-full relative overflow-hidden transform transition-transform hover:scale-105">
+              <span className="absolute top-0 left-0 w-full h-full bg-opacity-25 bg-white rounded-full transform scale-105 translate-x-2 translate-y-2"></span>
+              Profile <BsArrowRight className="ml-3 text-[#f1f0f3] text-lg" />
             </button>
           </div>
         </a>
